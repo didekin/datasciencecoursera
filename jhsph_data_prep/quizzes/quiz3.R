f86da76365b883d5901b51ad60280bc688982a95
Pedro Nevado <pnevado@javato.com>
2015-01-30 16:13:13 +0100

    Quizz 3 commit

--- jhsph_data_prep/quizzes/quiz3.R
+++ jhsph_data_prep/quizzes/quiz3.R
@@ -1,17 +1,17 @@
+# File with gdp data.
+
 fileUrl <-
     "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
 
 download.file(fileUrl,dest="./data/gdp1.csv",method="curl")
 
 gdp2 <- read.csv(file="./data/gdp1.csv"
-<<<<<<< HEAD
-                 ,header=F,skip=5
+                 ,skip=5
                  ,as.is=c(1,2,4,5)
-=======
-                 ,header=F,skip=5,as.is=c(1,2,4,5)
->>>>>>> 196425d6a835ebfd8b9725a57d05db05ca3bf8c4
+                 ,header=F
                  ,na.strings=c('""')
-                 ,nrows=215,strip.white=T
+                 ,nrows=215
+                 ,strip.white=T
                  ,skipNul=T
                  ,blank.lines.skip=T)
 
@@ -24,3 +24,42 @@ gdp2 <- gdp2[gdp2$CountryCode != "",]
 gdp2$gdp <- as.numeric(gsub(",","",gdp2$gdp))
 
 # File with education data.
+
+fileUrl <-
+  "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"
+
+download.file(fileUrl,dest="./data/edu1.csv",method="curl")
+
+edu2 <- read.csv(file="./data/edu1.csv"
+                 ,header=T
+                 ,as.is=c(1,2,3)
+                 ,na.strings=c('""')
+                 ,strip.white=T
+                 ,skipNul=T
+                 ,blank.lines.skip=T)
+
+edu2 <- edu2[edu2$CountryCode != "",c(1,3)]
+
+# Merge of the two files.
+
+library(plyr)
+gdp_edu <- arrange(join(gdp2,edu2),CountryCode)
+
+gedu_sort <- arrange(gdp_edu,desc(ranking))
+
+# Average GDP ranking for "High income: OECD" and "High income: nonOECD"
+# groups.
+
+gedu_sort$Income.Group <- factor(gedu_sort$Income.Group)
+
+gedu_avg1 <- ddply(gedu_sort,.(Income.Group),summarise,
+                   mean_ranking=mean(ranking,na.rm=T))
+
+# Cut the GDP ranking into 5 separate quantile groups. Make a table versus
+# Income.Group. How many countries are Lower middle income but among the 38
+# nations with highest GDP?
+
+library(Hmisc)
+gedu_sort$rankgroup <- cut2(gedu_sort$ranking,g=5)
+table(gedu_sort$rankgroup,useNA=c("no"))
+table(gedu_sort$rankgroup,gedu_sort$Income.Group)
